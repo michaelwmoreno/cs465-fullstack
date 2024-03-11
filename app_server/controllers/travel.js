ffa8c9ee3859eaced11af43d5295ad30b9ad97a0
michaelwmoreno <mikey56789@hotmail.com>
2024-03-11 18:27:28 -0500

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,3 +1,7 @@
+var fs = require('fs');
+var trips =
+JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
 /*GET travel view*/ 
 const travel = (req, res) => {
     res.render('travel', {title: 'Travlr Getaways'});
@@ -5,4 +9,4 @@ const travel = (req, res) => {
 
 module.exports = {
     travel
-}
+};
